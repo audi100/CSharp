0b104dad7272166f242e45bc006b182fa0e5bb33
Bankova <kristina.bankova@abv.bg>
2013-02-16 15:45:54 +0200

    Added ExceptionHandlingHomework

---
+++ ExceptionHandling/01. CalcSquareRoot/CalcSquareRoot.cs
@@ -0,0 +1,52 @@
+﻿//Write a program that reads an integer number and calculates and prints its square root.
+//If the number is invalid or negative, print "Invalid number". In all cases finally print "Good bye".
+//Use try-catch-finally.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class CalcSquareRoot
+{
+    static void Main()
+    {
+        Console.Write("Please enter positive integer:");
+        string integerNum = Console.ReadLine();
+        CalculateSquareRoot(integerNum);
+        
+    }
+    static void CalculateSquareRoot(string integerNum)
+    {
+        try
+        {
+            int number = int.Parse(integerNum);
+            if (number<0)
+            {
+                throw new FormatException();
+            }
+            Console.WriteLine("The square root is:{0}",(int)Math.Sqrt(number)); 
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number! ");
+
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Invalid number! ");
+
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Invalid number! ");
+
+        }
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+       
+    }
+}
+
---
+++ ExceptionHandling/02. CheckIntegerRange/CheckIntegerRange.cs
@@ -0,0 +1,50 @@
+﻿//Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. 
+//If an invalid number or non-number text is entered, the method should throw an exception. 
+//Using this method write a program that enters 10 numbers.
+
+using System;
+
+class CheckIntegerRange
+{
+    static void Main()
+    {
+        int countNumbers = 10;
+        int start = 1;
+        int end = 100;
+        Console.WriteLine("Please enter 10 integer numbers:");
+        for (int i = 0; i < countNumbers; i++)
+        {
+            ReadNumber(start, end);
+        }
+
+    }
+    static void ReadNumber(int start, int end)
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start || number >= end)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.WriteLine("The number is VALID.");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("The number in not in the range (1;100)"); 
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("The number in not in valid format!"); 
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("The number is too big !");
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("You have entered nothing!");
+        }
+    }
+}
+
