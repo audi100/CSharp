eab19faae51c1c12c0e0779a207661c9b3499dca
Bankova <kristina.bankova@abv.bg>
2013-03-05 19:37:04 +0200

    Added task one of the OOP-2 Homework

---
+++ OOP_Principles_Two/01. Shape/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+abstract class Shape
+{
+    private decimal width;
+    public decimal Width {get; set;}
+
+    private decimal heigth;
+    public decimal Heigth { get; set; }
+
+    public abstract decimal CalculateSurface();
+}
+
---
+++ OOP_Principles_Two/01. Shape/TestShapes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class TestShapes
+{
+    static void Main()
+    {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Triangle(10, 5);
+        shapes[1] = new Rectangle(4, 6);
+        shapes[2] = new Circle(5);
+
+        foreach (var s in shapes)
+        {
+            Console.WriteLine("The surface of {0} is {1:F2}.", s.GetType().Name, 
+                s.CalculateSurface());
+        }
+    }
+}
+
